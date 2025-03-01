a93108543013139313348ad37bcc1e0afce92069
SZlim <30948247+SZlim@users.noreply.github.com>
2025-03-01 22:10:45 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const a = 20;
+const b = 100;
+const myName = "Seoungjin";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello! " + myName);
+console.log("hi! " + myName);
