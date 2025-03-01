9ea69434fc77cedbd122ce8aed8fc8cbed2d970b
SZlim <30948247+SZlim@users.noreply.github.com>
2025-03-02 00:52:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const calculator = {
   plus: function (a, b) {
-    console.log(a + b);
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
   },
-  div: function (a, b) {
-    console.log(a / b);
+  divide: function (a, b) {
+    return a / b;
   },
   power: function (a, b) {
-    console.log(a ** b);
+    return a ** b;
   },
 };
 calculator.plus(5, 1);
 calculator.minus(5, 1);
-calculator.div(5, 1);
+calculator.divide(5, 1);
 calculator.power(5, 1);
+
+const plusResult = calculator.plus(5, 1);
+const minusResult = calculator.minus(plusResult, 1);
+const divideResult = calculator.divide(plusResult, minusResult);
+const powerResult = calculator.power(divideResult, plusResult);
+
+powerResult;
+minusResult;
+plusResult;
+divideResult;
