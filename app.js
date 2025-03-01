8084b7906fdfe097664001481c20af93bbee20ba
SZlim <30948247+SZlim@users.noreply.github.com>
2025-03-01 22:39:42 +0900

    Update app.js
    testing js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const a = 20;
 const b = 100;
 const myName = "Seoungjin";
+const toBuy = ["tomato", "potato", "pizza"];
 
+console.log(toBuy[2]);
+toBuy[2] = "water";
+console.log(toBuy[2]);
+toBuy.push("corn");
+console.log(toBuy[3]);
+console.log(toBuy);
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
