c3bcea9b5beb102242038850bc697841fd9384b7
SZlim <30948247+SZlim@users.noreply.github.com>
2025-03-02 23:31:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
-calculator.plus(5, 1);
-calculator.minus(5, 1);
-calculator.divide(5, 1);
-calculator.power(5, 1);
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(5, 1);
-const minusResult = calculator.minus(plusResult, 1);
-const divideResult = calculator.divide(plusResult, minusResult);
-const powerResult = calculator.power(divideResult, plusResult);
-
-powerResult;
-minusResult;
-plusResult;
-divideResult;
+console.log(isNaN(age));
+if (isNaN(age) || age < 0) {
+  console.log("Please write a positive number");
+} else if (age < 18) {
+  console.log("You are too young");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can dirink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age > 80) {
+  console.log("You can do whatever you want");
+}
