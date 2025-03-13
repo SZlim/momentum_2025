4b06e4f73fc2392c7a8d2e09680c97ff11886e6e
SZlim <30948247+SZlim@users.noreply.github.com>
2025-03-14 00:08:34 +0900

    Update weather.js

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "9ce72f66b259976e4507f0a43d9e61d6";
+
+function onGeoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} | ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can`t find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
