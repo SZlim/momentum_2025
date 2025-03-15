248ae913e33ee5d608bafebb781e9e6faae3010d
SZlim <30948247+SZlim@users.noreply.github.com>
2025-03-15 23:51:37 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,13 +1,4 @@
-const images = [
-  "01.jpg",
-  "02.jpg",
-  "03.jpg",
-  "04.jpg",
-  "05.jpg",
-  "06.jpg",
-  "07.jpg",
-  "08.jpg",
-];
+const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
