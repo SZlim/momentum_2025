af2c678e30623ff6aff9e58639b9979dee76f60a
jiin972 <jiin972@gmail.com>
2025-03-18 00:06:42 +0900

    Update greetings.js
    add hidden class for todo form

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const toDoForm = document.querySelector("#todo-form");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -17,6 +18,7 @@ function onLoginSubmit(event) {
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  toDoForm.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY); //첫 실행코드
@@ -27,5 +29,6 @@ if (savedUsername === null) {
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   //Show greeting
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   paintGreetings(savedUsername);
 }
