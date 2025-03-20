892e08b37372f989458c510509b164489b3100a5
jiin972 <jiin972@gmail.com>
2025-03-20 23:12:11 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -15,7 +15,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `Hello! ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
   todosForm.classList.remove(HIDDEN_CLASSNAME);
 }
